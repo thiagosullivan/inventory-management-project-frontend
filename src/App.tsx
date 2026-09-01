a5539da7c1d8923b30e939812d70f5cd69e66482
thiagosullivan <thiagosullivan12@gmail.com>
2026-09-01 00:47:30 -0300

    chore: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-red-500 text-2xl underline">Hello World</h1>
+      <Button>Click me</Button>
+      <Button size="xs" variant="outline">
+        Extra Small
+      </Button>
     </>
   );
 }
