5b8d9f406e0cbb74a8787326d32f4f9b864f0b60
thiagosullivan <thiagosullivan12@gmail.com>
2026-09-12 17:34:10 -0300

    feat: add Forgot Password page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
+import ForgotPassword from "./Pages/ForgotPassword";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
