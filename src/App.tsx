7b03722d661a218e76ca201fe0fb88ce3756adca
thiagosullivan <thiagosullivan12@gmail.com>
2026-09-12 17:37:06 -0300

    feat: add Dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import ForgotPassword from "./Pages/ForgotPassword";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
