e6f572bd915a003282d1f60c39243913278047bc
thiagosullivan <thiagosullivan12@gmail.com>
2026-09-17 12:07:10 -0300

    feat: add footer to root layout

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar";
-import Header from "../Header";
+import Header from "../header/Header";
+import Footer from "../Footer";
 
 export default function RootLayout() {
   return (
-    <div className="flex h-screen w-screen overflow-hidden bg-background text-foreground transition-colors duration-300 p-5">
+    <div className="flex h-screen w-screen overflow-x-hidden bg-background text-foreground transition-colors duration-300 p-5">
       <Sidebar />
       <main className="flex-1 pl-5">
         <Header />
         <Outlet />
+        <Footer />
       </main>
     </div>
   );
